2015f6e671c8e75d126609bdebbc379af2778e11
Ben Sullivan <benpetersullivan@gmail.com>
2017-11-05 13:25:56 +0000

    Initial Commit

---
+++ TideTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TideTracker
+//
+//  Created by Ben Sullivan on 05/11/2017.
+//  Copyright © 2017 Sullivan Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
